4cfe1e4d6e6ec4a494ecd26b7173a0b25dcde50b
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-22 05:28:07 +0300

    Created decoder for timestamps.

---
+++ TeaCoinAPI/TeaCoinAPI/Helpers/Decoders.swift
@@ -0,0 +1,19 @@
+//
+//  Decoders.swift
+//  TeaCoinAPI
+//
+//  Created by Hasanberk Yiğit on 22.01.2022.
+//
+
+import Foundation
+
+public enum Decoders {
+    //TODO: - Decoder for timestamps.
+//    static let plainDateDecoder: JSONDecoder = {
+//         //1330214400
+//        let decoder = JSONDecoder()
+//        let dateFormatter = DateFormatter()
+//        decoder.dateDecodingStrategy = .secondsSince1970
+//        return decoder
+//    }()
+}
