c6ca422a8a61127b70bbc784a622a0c9dd29d904
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-22 05:50:40 +0300

    Tested some cases.

--- TeaCoinAPI/TeaCoinAPITests/CoinTests.swift
+++ TeaCoinAPI/TeaCoinAPITests/CoinTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import TeaCoinAPI
 
 class CoinTests: XCTestCase {
-
+    
     func testParsing() throws {
         let bundle = Bundle(for: CoinTests.self)
         let url = bundle.url(forResource: "coin", withExtension: "json")
@@ -17,8 +17,17 @@ class CoinTests: XCTestCase {
         let data = try Data(contentsOf: url)
         let decoder = JSONDecoder()
         let coin = try decoder.decode(CoinsResponse.self, from: data)
-        print(coin)
+        
+        XCTAssertEqual(coin.data.coins.first?.uuid, "Qwsogvtv82FCd")
+        XCTAssertEqual(coin.data.coins.first?.symbol, "BTC")
+        XCTAssertEqual(coin.data.coins.first?.name, "Bitcoin")
+        XCTAssertEqual(coin.data.coins.first?.color, "#f7931A")
+        XCTAssertEqual(coin.data.coins.first?.iconUrl, URL(string: "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg"))
+        XCTAssertEqual(coin.data.coins.first?.marketCap, "1064845170034")
+        XCTAssertEqual(coin.data.coins.first?.price, "56373.67522635439")
+        XCTAssertEqual(coin.data.coins.first?.listedAt, 1330214400)
+        XCTAssertEqual(coin.data.coins.first?.change, "-3.61")
+        XCTAssertEqual(coin.data.coins.first?.sparkline[0], "58484.1797527997004874770000")
     }
-
     
 }
