5f63ed177df595ad6cf884608141802a8ce0c9e5
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-25 14:35:08 +0300

    ViewController updated.
    ViewModel injected.
    Created coinList array.
    Delegate setted.
    Nib registered.
    Cell created.

--- TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListViewController.swift
+++ TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListViewController.swift
@@ -11,27 +11,60 @@ final class CoinListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     
+    var viewModel: CoinListViewModelProtocol! {
+        didSet {
+            viewModel.delegate = self
+        }
+    }
+    
+    private var coinList: [CoinPresentation] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.title = "sa"
+        viewModel.load()
+        tableView.register(UINib(nibName: "CoinListCell", bundle: nil), forCellReuseIdentifier: "CoinListCell")
+        
+        
     }
+    
     
 }
 
-extension CoinListViewController: UITableViewDelegate, UITableViewDataSource {
+extension CoinListViewController: CoinListViewModelDelegate {
+    
+    func handleViewModelOutput(_ output: CoinListViewModelOutput) {
+        switch output {
+        case .updateTitle(let title):
+            self.title = title
+        case .setLoading(let isLoading):
+            UIApplication.shared.isNetworkActivityIndicatorVisible = isLoading
+        case .showCoinList(let coinList):
+            self.coinList = coinList
+            tableView.reloadData()
+        }
+    }
+    
+    func navigate(to route: CoinListViewRoute) {
+        //TODO: add to navigate func to delegate and change viewroute enum after created detail contracts.
+    }
+}
+
+extension CoinListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoinListCell", for: indexPath) as! CoinListCell
+        let coins = coinList[indexPath.row]
+        cell.cellConfigure(data: coins)
+        return cell
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return coinList.count
     }
     
-    
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
 }
-    
+
+extension CoinListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
     }
-
 }
