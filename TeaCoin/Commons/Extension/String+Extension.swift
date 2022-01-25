0f7bd28f72570bc91302664e7389f88e49a09443
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-26 00:39:22 +0300

    String extension revised.
    Fixed image problems.

--- TeaCoin/Commons/Extension/String+Extension.swift
+++ TeaCoin/Commons/Extension/String+Extension.swift
@@ -9,22 +9,20 @@ import Foundation
 
 extension String {
     
-   
-    public static func splitAtFirst(str: String, delimiter: String) -> (a: String, b: String)? {
-       guard let upperIndex = (str.range(of: delimiter)?.upperBound), let lowerIndex = (str.range(of: delimiter)?.lowerBound) else { return nil }
-       let firstPart: String = .init(str.prefix(upTo: lowerIndex))
-       let lastPart: String = .init(str.suffix(from: upperIndex))
-       return (firstPart, lastPart)
+    
+    public func svgToPng() -> String{
+        return self.replacingOccurrences(of: ".svg", with: ".png")
     }
     
-    public func svgToPng(_ string: String) -> String{
-         
-        let pngString = String.splitAtFirst(str: string, delimiter: "svg")
-        let firstPart = pngString?.a
-        guard let firstPart = firstPart else {return ""}
-        let toPng = "\(firstPart)png"
-        
-         return toPng
-     }
-    //TODO: toDouble
+    func toDouble() -> Double {
+        return Double(self) ?? 0.00
+    }
+    
+}
+
+extension Double {
+    
+    func percentageFormatter() -> String {
+        return String(format: "%.2f", self)
+    }
 }
