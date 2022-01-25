011b3d4c90ea3ceae96b1292a66a61af84e3a9d8
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-26 00:42:09 +0300

    Contracts updated.

--- TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListContracts.swift
+++ TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListContracts.swift
@@ -11,6 +11,8 @@ import Foundation
 protocol CoinListViewModelProtocol {
     var delegate: CoinListViewModelDelegate? { get set }
     func load()
+    func sortData()
+    func updateSelectedType(with listedType: ListedType)
     func selectCoin(at index: Int)
 }
 
@@ -18,12 +20,14 @@ enum CoinListViewModelOutput: Equatable {
     case updateTitle(String)
     case setLoading(Bool)
     case showCoinList([CoinPresentation])
+    case selectedType(ListedType)
 }
 
 enum CoinListViewRoute {
-    case detail(CoinListViewModelProtocol)
+    case detail(CoinDetailViewModelProtocol)
 }
 
-protocol CoinListViewModelDelegate: class {
+protocol CoinListViewModelDelegate: AnyObject {
     func handleViewModelOutput(_ output: CoinListViewModelOutput)
+        func navigate(to route: CoinListViewRoute)
 }
