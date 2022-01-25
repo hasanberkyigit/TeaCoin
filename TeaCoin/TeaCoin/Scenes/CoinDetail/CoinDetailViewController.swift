d3789d5fe16fc749febab091c4aa461e91717b9e
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-25 15:47:15 +0300

    CoinDetail storyboard and view controller created.

---
+++ TeaCoin/TeaCoin/Scenes/CoinDetail/CoinDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoinDetailViewController.swift
+//  TeaCoin
+//
+//  Created by Hasanberk Yiğit on 25.01.2022.
+//
+
+import UIKit
+
+class CoinDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
