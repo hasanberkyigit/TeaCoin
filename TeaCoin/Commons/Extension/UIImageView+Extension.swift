70592f14fcf4dce4442d2e12906b374f97859494
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-25 14:37:03 +0300

    Created a imageview extension for cell image.

---
+++ TeaCoin/Commons/Extension/UIImageView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageView+Extension.swift
+//  TeaCoin
+//
+//  Created by Hasanberk Yiğit on 25.01.2022.
+//
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    func setImage(with urlString: String){
+        guard let url = URL.init(string: urlString) else {
+            return
+        }
+        let resource = ImageResource(downloadURL: url, cacheKey: urlString)
+        var kf = self.kf
+        kf.indicatorType = .activity
+        self.kf.setImage(with: resource)
+    }
+}
