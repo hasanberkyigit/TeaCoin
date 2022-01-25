832a7aae942f44478d35e156b9274c033a9f0225
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-25 15:52:27 +0300

    IBOutlets connected.

--- TeaCoin/TeaCoin/Scenes/CoinDetail/CoinDetailViewController.swift
+++ TeaCoin/TeaCoin/Scenes/CoinDetail/CoinDetailViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class CoinDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    @IBOutlet weak var shortCodeLabel: UILabel!
+    @IBOutlet weak var changeRateLabel: UILabel!
+    @IBOutlet weak var highestPriceLabel: UILabel!
+    @IBOutlet weak var lowestPriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
