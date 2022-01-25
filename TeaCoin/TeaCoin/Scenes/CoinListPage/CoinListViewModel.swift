0c2623751b5ffa243fd9aabece3db77d6a41a870
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-26 00:41:40 +0300

    List type sort logic implemented.

--- TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListViewModel.swift
+++ TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListViewModel.swift
@@ -8,12 +8,16 @@
 import Foundation
 import TeaCoinAPI
 
+enum ListedType: String, CaseIterable {
+    case price = "Price", marketCap = "MarketCap", change = "Change", twentyFourHour = "24h Volume", listedAt = "ListedAt"
+}
 
 final class CoinListViewModel: CoinListViewModelProtocol {
   
     weak var delegate: CoinListViewModelDelegate?
     private let service: CoinsServiceProtocol
     private var coins: [CoinProperties] = []
+    private var selectedType: ListedType = .price
     
     init(service: CoinsServiceProtocol) {
         self.service = service
@@ -30,13 +34,34 @@ final class CoinListViewModel: CoinListViewModelProtocol {
             switch result {
             case .success(let response):
                 self.coins = response.coins
-                let presentations = response.coins.map({ CoinPresentation(coin: $0) })
-                self.notify(.showCoinList(presentations))
+                self.sortData()
             case .failure(let error):
                 print(error)
             }
         }
     }
+    func sortData(){
+        switch selectedType {
+        case .price:
+            self.coins = coins.sorted(by: {$0.price.toDouble() > $1.price.toDouble()})
+        case .change:
+            self.coins = coins.sorted(by: {$0.change.toDouble() > $1.change.toDouble()})
+        case .marketCap:
+            self.coins = coins.sorted(by: {$0.marketCap.toDouble() > $1.marketCap.toDouble()})
+        case .twentyFourHour:
+            self.coins = coins.sorted(by: {$0.twentyFourHourVolume.toDouble() > $1.twentyFourHourVolume.toDouble()})
+        case .listedAt :
+            self.coins = coins.sorted(by: {$0.listedAt > $1.listedAt})
+        }
+
+        notify(.selectedType(selectedType))
+        notify(.showCoinList(coins.map({ CoinPresentation(coin: $0) })))
+    }
+    
+    func updateSelectedType(with listedType: ListedType) {
+        self.selectedType = listedType
+    }
+   
     
     func selectCoin(at index: Int) {
         let coin = coins[index]
