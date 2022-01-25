0ba0b415cce49a9f00a2f20e792bb391b3774005
Hasanberk Yigit <hasanberkyigit@gmail.com>
2022-01-25 14:04:08 +0300

    CoinListCell created.
    Custom cell created.

--- TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListCell.swift
+++ TeaCoin/TeaCoin/Scenes/CoinListPage/CoinListCell.swift
@@ -5,3 +5,44 @@
 //  Created by Hasanberk Yiğit on 24.01.2022.
 //
 
+import UIKit
+import Kingfisher
+
+final class CoinListCell: UITableViewCell {
+    
+    @IBOutlet weak var coinImage: UIImageView!
+    @IBOutlet weak var shortCodeLabel: UILabel!
+    @IBOutlet weak var coinNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceChangeLabel: UILabel!
+    
+    private let pngString:String = ""
+    
+    private var coinList: [CoinPresentation] = []
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func cellConfigure(data: CoinPresentation){
+        
+        let str = pngString.svgToPng(data.icon)
+        coinImage.setImage(with: str )
+        shortCodeLabel.text = data.symbol
+        coinNameLabel.text = data.name
+        priceLabel.text = data.price
+        //TODO: Change me
+        if Double(data.change) ?? 0 < 0 {
+            priceChangeLabel.textColor = UIColor.red
+            shortCodeLabel?.text = data.symbol
+        }else {
+            priceChangeLabel.textColor = UIColor.green
+            shortCodeLabel?.text = data.symbol
+        }
+    }
+}
+
