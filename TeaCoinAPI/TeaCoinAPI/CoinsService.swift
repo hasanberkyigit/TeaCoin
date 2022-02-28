9006d5433e9de51461cc314b172a31cc10da45f9
Hasanberk Yigit <39728257+hasanberkyigit@users.noreply.github.com>
2022-02-28 23:41:51 +0300

    Indentation fixed.

--- TeaCoinAPI/TeaCoinAPI/CoinsService.swift
+++ TeaCoinAPI/TeaCoinAPI/CoinsService.swift
@@ -20,22 +20,22 @@ public class CoinsService: CoinsServiceProtocol{
     }
     public init() {}
     public func fetchCoins(completion: @escaping (Result<CoinResponse>) -> Void) {
-       
+        
         let urlString = "https://psp-merchantpanel-service-sandbox.ozanodeme.com.tr/api/v1/dummy/coins"
         
         AF.request(urlString).responseData { (response) in
-                    switch response.result {
-                    case .success(let data):
-                        let decoder = Decoders.plainDateDecoder
-                        do {
-                            let response = try decoder.decode(CoinResponse.self, from: data)
-                            completion(.success(response))
-                        } catch {
-                            completion(.failure(Error.serializationError(internal: error)))
-                        }
-                    case .failure(let error):
-                        completion(.failure(Error.networkError(internal: error)))
-                    }
+            switch response.result {
+            case .success(let data):
+                let decoder = Decoders.plainDateDecoder
+                do {
+                    let response = try decoder.decode(CoinResponse.self, from: data)
+                    completion(.success(response))
+                } catch {
+                    completion(.failure(Error.serializationError(internal: error)))
                 }
+            case .failure(let error):
+                completion(.failure(Error.networkError(internal: error)))
             }
         }
+    }
+}
